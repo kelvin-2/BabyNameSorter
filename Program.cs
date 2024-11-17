9b3cdf2085e1ad462bdcd6193554130d5d48957a
kelvin-Mudzingwa <106997104+kelvin-2@users.noreply.github.com>
2024-11-17 11:45:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace Comparators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        public Program()
+        {
+            ReadAndCheckLines();
+        }
+
+        public void ReadAndCheckLines()
+        {
+
+            using (StreamReader sr = new StreamReader("TestInputs.txt"))
+            {
+                string line;
+                int lineNumber = 1; // Track the line number for user clarity
+                while ((line = sr.ReadLine()) != null)
+                {
+                    // Split the line into parts
+                    string[] parts = line.Split(',');
+
+                    // Check if the line is sorted
+                    if (IsSorted(parts))
+                    {
+                        Console.WriteLine($"Line {lineNumber}: Sorted");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Line {lineNumber}: Unsorted");
+                    }
+                    lineNumber++;
+                }
+            }
+        }
+
+        public bool IsSorted(string[] parts)
+        {
+            for (int i = 0; i < parts.Length - 1; i++)
+            {
+                
+                if (parts[i].CompareTo(parts[i + 1]) > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
